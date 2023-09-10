afb87206e2e4bb6eaca8613853f7960efe85ceb5
mustkimkureshi <mustkimkureshi34@gmail.com>
2023-09-10 00:08:47 -0700

    cpp

---
+++ lesson2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    // Declare a variable named 'number' of type int
+    int number;
+
+    // Assign a value to the 'number' variable
+    number = 42;
+
+    // Print the value of the 'number' variable
+    std::cout << "The value of 'number' is: " << number << std::endl;
+
+    return 0;
+}
